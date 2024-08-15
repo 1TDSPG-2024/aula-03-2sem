3c3b4a94dbf1bee07d81244a7afb818568369dfc
mateuscastronappe <teteucastro31@gmail.com>
2024-08-15 12:15:11 -0300

    Estrutura de pasta e iniciando componentinazação e limpeza boyler plate

---
+++ aula03/src/App.jsx
@@ -0,0 +1,18 @@
+/**
+ * Regras para um componente funcional do React.
+ * 1 - Ser uma função JavaScript.
+ * 2 - Esta função deve ter como uma boa prática o mesmo nome do arquivo em questão.
+ * 3 - Ter um instrução return com corpo. Ex return( ... )
+ * 4 - Utilizar as directivas de exportação: export default antes do nome function.
+ * 5 - Criar uma estrutura dentro do return com elementos de blocos HTML do tipo PAI Ex: DIV,HEADER, MAIN, SECTION...
+ * 6 - Opcional utilizar o "import React from 'react';", somente se utilizar alguma funcionalidade do pacote 'react'.
+**/
+ 
+ 
+export default function App(){
+ return(
+<div>
+  <h1> olá mundo</h1>
+</div>
+  );
+}
