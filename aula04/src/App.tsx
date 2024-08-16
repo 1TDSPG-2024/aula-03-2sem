1f2538ecd31cbc1d6bcb8efc51686171be6abe21
ntncaue <cauearaujo60@gmail.com>
2024-08-16 09:36:37 -0300

    Limpeza do boyler, componentização, modularização, listage, inferência de props

---
+++ aula04/src/App.tsx
@@ -0,0 +1,35 @@
+import Cabecalho from "./components/Cabecalho/Cabecalho";
+import Conteudo from "./components/Conteudo/Conteudo";
+import Rodape from "./components/Rodape/Rodape";
+function App() {
+  return (
+    <div>
+      <Cabecalho />
+      <Conteudo />
+      <Rodape />
+    </div>
+  );
+}
+
+export default App;
+// export default function App() {
+//   return (
+//     <div>App</div>
+//   )
+// }
+
+
+
+
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>Componente App</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
